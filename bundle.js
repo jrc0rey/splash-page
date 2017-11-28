8c87748fdabfe75857516624d280b4813b694063
Jason Corey <jason@Jasons-MacBook-Air.local>
2017-11-28 06:50:04 -0600

    third commit

--- bundle.js
+++ bundle.js
@@ -5,7 +5,10 @@ var Typed = require('typed.js');
 var options = {
   strings: ["Improvements coming. ^1000 </br> Check back soon."],
   typeSpeed: 60,
-  loop: true
+  loop: true,
+  backspeed: 1000,
+  backDelay: 2000,
+  startDelay: 1000
 }
 
 var typed = new Typed(".line", options);
