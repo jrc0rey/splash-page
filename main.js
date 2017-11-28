925c96d6d0afcd6d0591ec6d7f5a1900f0a7fb7f
Jason Corey <jason@Jasons-MacBook-Air.local>
2017-11-27 20:21:09 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var unique = require('uniq');
+var Typed = require('typed.js');
+
+var options = {
+  strings: ["Improvements Coming. ^1000 Check back soon."],
+  typeSpeed: 60,
+  loop: true
+}
+
+var typed = new Typed(".line", options);
