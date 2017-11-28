89cd682e4fa319d1f369a05cefb068755eba3e2a
Jason Corey <jason@Jasons-MacBook-Air.local>
2017-11-27 22:24:39 -0600

    second commit

--- bundle.js
+++ bundle.js
@@ -3,7 +3,7 @@ var unique = require('uniq');
 var Typed = require('typed.js');
 
 var options = {
-  strings: ["Improvements Coming. ^1000 Check back soon."],
+  strings: ["Improvements coming. ^1000 </br> Check back soon."],
   typeSpeed: 60,
   loop: true
 }
